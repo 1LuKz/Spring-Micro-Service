ae790502df974b7572a1675f0bf016b1e0ae6282
1LuKz <lucasedu292003@gmail.com>
2025-11-23 15:06:54 -0300

    Feign client

---
+++ hr-payroll/src/main/java/com/springmicroservice/hr_payroll/feignclients/WorkerFeignClient.java
@@ -0,0 +1,14 @@
+package com.springmicroservice.hr_payroll.feignclients;
+
+import com.springmicroservice.hr_payroll.entities.Worker;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(name = "hr-worker", url = "http://localhost:8001", path = "/workers")
+public interface WorkerFeignClient {
+
+    @GetMapping("/{id}")
+    ResponseEntity<Worker> findById(@PathVariable("id") Integer id);
+}
